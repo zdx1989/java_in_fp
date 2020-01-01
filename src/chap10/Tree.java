4821c4cbe94ac4396b2b045d4b292d73de3e5aa7
zhoudunxiong <zhoudunxiong123@foxmail.com>
2020-01-01 22:21:35 +0800

    tree advance

--- src/chap10/Tree.java
+++ src/chap10/Tree.java
@@ -1,5 +1,6 @@
 package chap10;
 
+import chap2.Function;
 import chap5.List;
 import chap6.Option;
 
@@ -33,6 +34,20 @@ public abstract class Tree<A extends Comparable<A>> {
     abstract Tree<A> remove(A ra);
 
     abstract Tree<A> merge(Tree<A> ta);
+
+    abstract <B> B foldLeft(B identity, Function<B, Function<A, B>> f, Function<B, Function<B, B>> g);
+
+    abstract <B> B foldInOrder(B identity, Function<B, Function<A, Function<B, B>>> f);
+
+    abstract <B> B foldPreOrder(B identity, Function<A, Function<B,Function<B, B>>> f);
+
+    abstract <B> B foldPostOrder(B identity, Function<B, Function<B, Function<A, B>>> f);
+
+    abstract Tree<A> rotateLeft();
+
+    abstract Tree<A> rotateRight();
+
+    abstract List<A> toListInOrderRight();
 
     private static class Empty<A extends Comparable<A>> extends Tree<A> {
         @Override
@@ -98,6 +113,41 @@ public abstract class Tree<A extends Comparable<A>> {
         @Override
         Tree<A> merge(Tree<A> ta) {
             return ta;
+        }
+
+        @Override
+        <B> B foldLeft(B identity, Function<B, Function<A, B>> f, Function<B, Function<B, B>> g) {
+            return identity;
+        }
+
+        @Override
+        <B> B foldInOrder(B identity, Function<B, Function<A, Function<B, B>>> f) {
+            return identity;
+        }
+
+        @Override
+        <B> B foldPreOrder(B identity, Function<A, Function<B, Function<B, B>>> f) {
+            return identity;
+        }
+
+        @Override
+        <B> B foldPostOrder(B identity, Function<B, Function<B, Function<A, B>>> f) {
+            return identity;
+        }
+
+        @Override
+        Tree<A> rotateLeft() {
+            return this;
+        }
+
+        @Override
+        Tree<A> rotateRight() {
+            return this;
+        }
+
+        @Override
+        List<A> toListInOrderRight() {
+            return List.list();
         }
     }
 
@@ -196,6 +246,48 @@ public abstract class Tree<A extends Comparable<A>> {
                         : ta.value().compareTo(value) > 0
                             ? new T<>(right, value, right.merge(new T<>(empty(), ta.value(), ta.right()))).merge(ta.left())
                             : new T<>(left.merge(ta.left()), value, right.merge(ta.right()));
+        }
+
+        @Override
+        <B> B foldLeft(B identity, Function<B, Function<A, B>> f, Function<B, Function<B, B>> g) {
+            return g.apply(left.foldLeft(identity, f, g)).apply(f.apply(right.foldLeft(identity, f, g)).apply(value));
+        }
+
+        //left -> value -> right
+        @Override
+        <B> B foldInOrder(B identity, Function<B, Function<A, Function<B, B>>> f) {
+            return f.apply(left.foldInOrder(identity, f)).apply(value).apply(right.foldInOrder(identity, f));
+        }
+
+        //value -> left -> right
+        @Override
+        <B> B foldPreOrder(B identity, Function<A, Function<B, Function<B, B>>> f) {
+            return f.apply(value).apply(left.foldPreOrder(identity, f)).apply(right.foldPreOrder(identity, f));
+        }
+
+        //left -> right -> value
+        @Override
+        <B> B foldPostOrder(B identity, Function<B, Function<B, Function<A, B>>> f) {
+            return f.apply(left.foldPostOrder(identity, f)).apply(right.foldPostOrder(identity, f)).apply(value);
+        }
+
+        @Override
+        Tree<A> rotateLeft() {
+            return right.isEmpty()
+                    ? this
+                    : new T<>(new T<>(left, value, right.left()), right.value(), right.right());
+        }
+
+        @Override
+        Tree<A> rotateRight() {
+            return left.isEmpty()
+                    ? this
+                    : new T<>(left.left(), left.value(), new T<>(left.right(), value, right));
+        }
+
+        @Override
+        List<A> toListInOrderRight() {
+            return List.concat(List.concat(left.toListInOrderRight(), List.list(value)), right.toListInOrderRight());
         }
     }
 
